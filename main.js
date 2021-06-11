0a90d633e01f17bc1ff2946f5061845189da0979
Nixon <nixonkurian.nk@gmail.com>
2021-06-11 22:32:28 +0530

    few commands

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+require("dotenv").config();
 
-const prefix = "nix";
+const prefix = "pp";
 
 const fs = require("fs");
 
@@ -20,7 +21,7 @@ client.once("ready", () => {
   console.log("Nix-bot is online!");
 });
 
-// this for test purpose
+// this is for test purpose
 // client.on("message", (message) => {
 //   if (message.content.includes("468694108945383434")) {
 //     message.channel.send("Nixon is currently not available");
@@ -28,21 +29,80 @@ client.once("ready", () => {
 // });
 
 client.on("message", (message) => {
-  let args = message.content.substring(prefix.length).split(" ");
+  let args = message.content.substring(prefix.length + 1).split(" ");
+
   let author = message.author.username;
 
   if (message.content.includes("468694108945383434")) {
     message.channel.send("Onii-chan is currently not available");
   }
+  if (message.content.includes("684683991936532492")) {
+    message.channel.send("Baka   is currently not available");
+  }
   switch (args[0]) {
     case "help":
       // to use embed > add RichEmbed on the first line of code const {Client,RichEmbed} = require(Discord.js);
-      //   const Embed = new RichEmbed()
-      //     .setTitle("Helper Embed")
-      //     .setColor(0xff0000)
-      //     .setDescription("Whatever the message you want");
+      const embed = new Discord.MessageEmbed()
+        .setColor("#0099ff")
+        .setTitle(
+          `${author}`,
+          `${message.author.displayAvatarURL({
+            format: "png",
+            dynamic: true,
+          })}`
+        )
+        .setAuthor("Henlo")
+        .setDescription("pp bot is a fun bot")
+        .setThumbnail(
+          "https://cdn.discordapp.com/avatars/468694108945383434/a_c52d212378f8073e7bb5aae3f042c7e8.gif"
+        )
+        .addFields(
+          {
+            name: "Still Under development!",
+            value: "\u200B",
+          },
+          { name: "Following are the commands:", value: "\u200B" },
+          {
+            name: "pp hi ",
+            value: "Greetings",
+            inline: true,
+          },
+          {
+            name: "pp avatar",
+            value: "Display user avatar",
+            inline: true,
+          },
+          { name: "\u200B", value: "\u200B" },
+          {
+            name: "pp ping",
+            value: "Pongs back!",
+            inline: true,
+          },
+          {
+            name: "pp beep",
+            value: "Boops back!",
+            inline: true,
+          },
+          { name: "\u200B", value: "\u200B" }
+        )
+        .addField(
+          `Hope you got the commands ${message.author.username}`,
+          "Have fun!!"
+        )
+        .setImage(
+          "https://www.elyrsps.net/forums/uploads/monthly_2021_03/image0.thumb.gif.0b892035b2937c2572355a72f222171c.gif"
+        )
+        .setTimestamp()
+        .setFooter(
+          "pp-bot",
+          "https://pa1.narvii.com/6831/2b060dd90cd5ddbf28111304ac9b0c9b81fda044_128.gif"
+        );
 
-      message.author.send(`Hello ${author}, this is Nix-bot`);
+      message.author.send(embed);
+
+      // message.author.send(
+      //   `Hello ${author}, this is Nix-bot.\nTo use commands add prefix - pp and the command.\nFor eg. pp hi.\nThe commands are hi,avatar,ping,beep`
+      // );
       break;
 
     case "avatar":
@@ -89,4 +149,4 @@ client.on("message", (message) => {
 //   }
 // });
 
-client.login("NzcyODAyMDAyMjc4NTQ3NDk2.X5_98w.9Fbr6gBmL6A2mhq2KAgr5aj6DCY");
+client.login(process.env.TOKEN);
